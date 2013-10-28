50bec2ff96b118a5695526f16c45b630062f5403
bdjackson <bjack3@gmail.com>
2013-10-28 10:59:57 +0100

    update get_vim_bundles script

--- vim/bundle/get_vim_bundles.sh
+++ vim/bundle/get_vim_bundles.sh
@@ -1,25 +1,39 @@
-# install NERDTree bundle which provides a nice tree structure for viewing files
+echo ''
+echo '------------------------------------------------------------'
+echo 'install NERDTree bundle which provides a nice tree structure for viewing files'
 git clone git://github.com/scrooloose/nerdtree.git
 
-# install commandT bundle used for fast opening of files within a project
+echo ''
+echo '------------------------------------------------------------'
+echo 'install commandT bundle used for fast opening of files within a project'
 git clone git://git.wincent.com/command-t.git
 cd command-t/ruby/command-t
 ruby extconf.rb
 make 
 cd - 
 
-# install snipmate bundle used for textmate style snippits 
+echo ''
+echo '------------------------------------------------------------'
+echo 'install snipmate bundle used for textmate style snippits'
 git clone git://github.com/msanders/snipmate.vim.git
 git clone git://github.com/scrooloose/snipmate-snippets.git scrooloose_snippets
 
-# install super tab bundle
+echo ''
+echo '------------------------------------------------------------'
+echo 'install super tab bundle
 git clone git://github.com/ervandew/supertab.git
 
-# install OmniCppComplete bundle for contextual c++ completion
+echo ''
+echo '------------------------------------------------------------'
+echo 'install OmniCppComplete bundle for contextual c++ completion'
 git clone git://github.com/vim-scripts/OmniCppComplete.git
 
-# install unimpaired bundle for "text bubbling"
+echo ''
+echo '------------------------------------------------------------'
+echo 'install unimpaired bundle for "text bubbling"'
 git clone git://github.com/tpope/vim-unimpaired.git
 
-# install tabular for cool alignment
+echo ''
+echo '------------------------------------------------------------'
+echo 'install tabular for cool alignment'
 git clone git://github.com/godlygeek/tabular.git
