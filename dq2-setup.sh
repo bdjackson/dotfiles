3fb7b35fc2a6afc9d2aad22137b3127a8c149a6a
bdjackson <bjack3@gmail.com>
2014-09-17 17:22:03 +0200

    up dq2-setup.sh

--- dq2-setup.sh
+++ dq2-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-source /afs/cern.ch/project/gd/LCG-share/current/external/etc/profile.d/grid-env.sh
+
+setupATLAS
+sleep 1
+localSetupDQ2Client
 sleep 1
 voms-proxy-init -voms atlas
-sleep 1
-source /afs/cern.ch/atlas/offline/external/GRID/ddm/DQ2Clients/setup.sh
-
