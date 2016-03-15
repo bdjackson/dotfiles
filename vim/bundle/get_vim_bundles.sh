a4ba1631e9c08d3a9358dfbe677a365e8e472616
Jackson <bjackson@2usmbjackson.local>
2016-03-15 09:31:39 -0500

    update script to get vim bundles

--- vim/bundle/get_vim_bundles.sh
+++ vim/bundle/get_vim_bundles.sh
@@ -1,33 +1,30 @@
 git clone https://github.com/VundleVim/Vundle.vim.git 
 
+echo ''
+echo '------------------------------------------------------------'
+echo 'install NERDTree bundle which provides a nice tree structure for viewing files'
+git clone git://github.com/scrooloose/nerdtree.git
 
+echo ''
+echo '------------------------------------------------------------'
+echo 'install commandT bundle used for fast opening of files within a project'
+git clone git://git.wincent.com/command-t.git
+cd command-t/ruby/command-t
+ruby extconf.rb
+make
+cd -
 
+echo ''
+echo '------------------------------------------------------------'
+echo 'install snipmate bundle used for textmate style snippits'
+git clone git://github.com/msanders/snipmate.vim.git
+git clone git://github.com/scrooloose/snipmate-snippets.git scrooloose_snippets
 
-# echo ''
-# echo '------------------------------------------------------------'
-# echo 'install NERDTree bundle which provides a nice tree structure for viewing files'
-# git clone git://github.com/scrooloose/nerdtree.git
-# 
-# # echo ''
-# # echo '------------------------------------------------------------'
-# # echo 'install commandT bundle used for fast opening of files within a project'
-# # git clone git://git.wincent.com/command-t.git
-# # cd command-t/ruby/command-t
-# # ruby extconf.rb
-# # make 
-# # cd - 
-# 
-# echo ''
-# echo '------------------------------------------------------------'
-# echo 'install snipmate bundle used for textmate style snippits'
-# git clone git://github.com/msanders/snipmate.vim.git
-# git clone git://github.com/scrooloose/snipmate-snippets.git scrooloose_snippets
-# 
-# echo ''
-# echo '------------------------------------------------------------'
-# echo 'install super tab bundle'
-# git clone git://github.com/ervandew/supertab.git
-# 
+echo ''
+echo '------------------------------------------------------------'
+echo 'install super tab bundle'
+git clone git://github.com/ervandew/supertab.git
+
 # echo ''
 # echo '------------------------------------------------------------'
 # echo 'install OmniCppComplete bundle for contextual c++ completion'
@@ -37,8 +34,8 @@ git clone https://github.com/VundleVim/Vundle.vim.git
 # echo '------------------------------------------------------------'
 # echo 'install unimpaired bundle for "text bubbling"'
 # git clone git://github.com/tpope/vim-unimpaired.git
-# 
-# echo ''
-# echo '------------------------------------------------------------'
-# echo 'install tabular for cool alignment'
-# git clone git://github.com/godlygeek/tabular.git
+
+echo ''
+echo '------------------------------------------------------------'
+echo 'install tabular for cool alignment'
+git clone git://github.com/godlygeek/tabular.git
